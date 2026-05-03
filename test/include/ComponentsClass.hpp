a3890fd4bbb2e89e46eb21a20c79905e665dc96b
semenshestakov <semenshestakov1703@gmail.com>
2026-05-03 19:57:31 +0300

    [v0.10.2] [ecs] test leaks: good

--- test/include/ComponentsClass.hpp
+++ test/include/ComponentsClass.hpp
@@ -9,6 +9,53 @@ struct Position2d final
 struct Position3d final
 {
     float x {1}, y {2}, z{3};
+    void* testLeaks = nullptr;
+
+    Position3d() : Position3d(1, 2, 3) {}
+    Position3d(const float x) : Position3d(x, 2.f, 3.f) {}
+    Position3d(const float x, const float y) : Position3d(x, y, 3.f) {}
+    Position3d(const float x, const float y, const float z) : x(x), y(y), z(z)
+    {
+        testLeaks = malloc(sizeof(Position3d));
+    }
+
+    Position3d(const Position3d& other)
+    {
+        x = other.x;
+        y = other.y;
+        z = other.z;
+    }
+
+    Position3d(Position3d&& other) noexcept
+    {
+        std::swap(testLeaks, other.testLeaks);
+        std::swap(x, other.x);
+        std::swap(y, other.y);
+        std::swap(z, other.z);
+    }
+
+    Position3d& operator=(const Position3d& other)
+    {
+        testLeaks = malloc(sizeof(Position3d));
+        x = other.x;
+        y = other.y;
+        z = other.z;
+        return *this;
+    }
+
+    Position3d& operator=(Position3d&& other) noexcept
+    {
+        std::swap(testLeaks, other.testLeaks);
+        std::swap(x, other.x);
+        std::swap(y, other.y);
+        std::swap(z, other.z);
+        return *this;
+    }
+
+    ~Position3d()
+    {
+        free(testLeaks);
+    }
 };
 
 struct DestructorTest final
